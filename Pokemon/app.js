391a10435968ed9df10adee77da986fd5a8dcebe
User <user@Users-Air.fios-router.home>
2020-02-12 06:59:42 -0500

    pokemon api

--- Pokemon/app.js
+++ Pokemon/app.js
@@ -1,1 +1,24 @@
-console.log('hello');
+
+$(() => {
+    $('form').on('submit', (event)=>{
+
+        event.preventDefault();
+
+        const userInput = $('input[type="text"]').val();
+
+        $.ajax({
+            url:'https://pokeapi.co/api/v2/pokemon/' + userInput
+        }).then(
+            (data)=>{
+                $('#name').html(data.forms[0].name);
+                $('#ability').html(data.abilities[0].ability.name);
+                $('#base_experience').html(data.base_experience);
+                // $('input[type="text"]').empty(); for removing html elements
+                $('form').trigger('reset') // in order to empty the values so it depends on what you need and how your form works
+            },
+            ()=>{
+                console.log('bad');
+            }
+        );
+    })
+})
